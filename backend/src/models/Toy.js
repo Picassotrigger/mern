3d40e1394a3586ca4ab58cc1d93f17ab11bf9035
David DuChene <davidduchene@Davids-MacBook-Pro.local>
2017-09-08 11:45:51 -0500

    toy model

---
+++ backend/src/models/Toy.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+//define schema for toys
+var Toy = new Schema({
+  toy: {
+    type: String
+  }
+}, {
+  collection: 'toys'
+});
+
+
+module.exports = mongoose.model('Toy', Toy)
